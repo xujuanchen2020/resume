0478d164b1e65ddb0c4ec0a9e31f9f4077fc66b6
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-28 14:20:42 -0500

    grid & layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Container, Grid } from "@material-ui/core";
+import Profile from './components/Profile/Profile.js';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import Portfolio from './pages/Portfolio/Portfolio.js';
+import Resume from './pages/Resume/Resume.js'
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg ={3} style={{background:"red"}}>
+          <Profile/>
+        </Grid>
+        <Grid item xs style={{background:"blue"}}>
+          <Header/>
+          <Portfolio/>
+          <Resume/>
+          <Footer/>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
