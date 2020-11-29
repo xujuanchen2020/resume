bdb3c3e1abba4f3a7b1db987bfd69195fd5a3cc7
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-29 11:12:11 -0500

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './FooterStyle.css';
+import Typography from '@material-ui/core/Typography';
+import reuseData from '../../utils/reuseData';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='footer'>
+            <div className='footer_left'>
+    <Typography className='footer_name'>{reuseData.name}</Typography>
+            </div>
+            <div className='footer_right'>
+                <Typography className='footer_copyright'>
+                    Designed and Developed by Xujuan Chen
+                    <br/>
+                    Clone idea from 
+                    <a href='https://themeforest.net/user/tavonline' 
+                    target='_blank'> tavonline</a>
+                </Typography>
+            </div>
         </div>
     )
 }
